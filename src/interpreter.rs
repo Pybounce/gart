13e65d8067aa605b2376d5238f38e8645cfeb852
Pybounce <pybounce@protonmail.com>
2025-12-30 03:23:52 +0000

    adding many native functions

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,6 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{io::{self, Write}, rc::Rc, time::{SystemTime, UNIX_EPOCH}};
+
+use rand::Rng;
 
 use crate::{compiler::Compiler, value::{NativeFunction, Value}, vm::VM};
 
@@ -86,6 +88,107 @@ impl Interpreter {
             },
         };
 
+        let random_range = NativeFunction {
+            name: "random_range".to_owned(),
+            arity: 2,
+            function: {
+                fn random_range(vals: &[Value]) -> Value {
+                    return match (vals[0].clone(), vals[1].clone()) {
+                        (Value::Number(min), Value::Number(max)) => {
+                            let mut rng = rand::rng();
+                            return Value::Number(rng.random_range(min..=max) as f64);
+                        },
+                        _ => Value::Null
+                    };
+
+                }
+
+                Box::new(random_range)
+            },
+        };
+
+        let to_number = NativeFunction {
+            name: "number".to_owned(),
+            arity: 1,
+            function: {
+                fn number(vals: &[Value]) -> Value {
+                    return match vals[0].clone() {
+                        Value::Number(number) => Value::Number(number),
+                        Value::String(string) => match string.parse::<f64>() {
+                            Ok(number) => Value::Number(number),
+                            Err(_) => Value::Null,
+                        },
+                        _ => Value::Null
+                    };
+                }
+                Box::new(number)
+            },
+        };
+
+        let to_string = NativeFunction {
+            name: "string".to_owned(),
+            arity: 1,
+            function: {
+                fn to_string(vals: &[Value]) -> Value {
+                    match vals[0].clone() {
+                        Value::String(s) => Value::String(s),
+                        Value::Number(n) => Value::String(Rc::new(n.to_string())),
+                        Value::Bool(b) => Value::String(Rc::new(b.to_string())),
+                        Value::Null => Value::String(Rc::new("null".to_owned())),
+                        _ => Value::Null,
+                    }
+                }
+                Box::new(to_string)
+            },
+        };
+
+        let get_input = NativeFunction {
+            name: "input".to_owned(),
+            arity: 1,
+            function: {
+                fn input(vals: &[Value]) -> Value {
+                    print!("{}", vals[0].clone());
+                    io::stdout().flush().unwrap();
+                    let mut input = String::new();
+                    io::stdin().read_line(&mut input).unwrap();
+                    return Value::String(Rc::new(input.trim().to_string()));
+                }
+                Box::new(input)
+            },
+        };
+
+        let clear = NativeFunction {
+            name: "clear".to_owned(),
+            arity: 0,
+            function: {
+                fn clear(_: &[Value]) -> Value {
+                    print!("\x1B[2J\x1B[1;1H"); // basically regex so who the fuck knows
+                    return Value::Null;
+                }
+                Box::new(clear)
+            },
+        };
+
+        let round = NativeFunction {
+            name: "round".to_owned(),
+            arity: 1,
+            function: {
+                fn round(vals: &[Value]) -> Value {
+                    return match vals[0].clone() {
+                        Value::Number(num) => Value::Number(num.round()),
+                        _ => Value::Null
+                    };
+                }
+                Box::new(round)
+            },
+        };
+
         compiler.add_native(time);
         compiler.add_native(print);
+        compiler.add_native(random_range);
+        compiler.add_native(to_number);
+        compiler.add_native(to_string);
+        compiler.add_native(get_input);
+        compiler.add_native(clear);
+        compiler.add_native(round);
     }
